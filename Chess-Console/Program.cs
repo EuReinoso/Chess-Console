29a58fd3bae58b6f23b2cc609ac138c83a280ddf
EuReinoso <lucas.reinoso.br@gmail.com>
2021-01-07 15:49:33 -0300

    Class Position

--- Chess-Console/Program.cs
+++ Chess-Console/Program.cs
@@ -6,7 +6,7 @@ namespace Chess_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ Chess-Console/table/position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess_Console.table
+{
+    class Position
+    {
+        public int lin { get; set; }
+        public int col { get; set; }
+
+        public Position(int lin, int col)
+        {
+            this.lin = lin;
+            this.col = col;
+        }
+    }
+}
